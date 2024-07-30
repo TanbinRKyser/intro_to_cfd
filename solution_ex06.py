15cce31219f6fb60cef117040e3d742713a0e52a
TanbinRKyser <tmitul515@gmail.com>
2024-07-30 17:30:32 +0200

    300724 -- updated codde for exercise 7

--- solution_ex06.py
+++ solution_ex06.py
@@ -30,15 +30,13 @@ y_arr = [y]
 z_arr = [z]
 t_arr = [t]
 
-
- 
 #%%
 # TIME LOOP
-while(t<t_end):
+while( t < t_end ):
     ## UPDATE THE NEW = OLD + UPDATE
     x_new = x + dt * ( s * ( y - x ) )
-    y_new = y + dt * ( (( r - z ) * x) - y )
-    z_new = z + dt * ( (x * y) - ( b * z ) )
+    y_new = y + dt * ( ( ( r - z ) * x ) - y )
+    z_new = z + dt * ( ( x * y ) - ( b * z ) )
     
     x = x_new
     y = y_new
@@ -47,94 +45,84 @@ while(t<t_end):
     n += 1
     
     # data storage
-    x_arr.append(x)
-    y_arr.append(y)
-    z_arr.append(z)
+    x_arr.append( x )
+    y_arr.append( y )
+    z_arr.append( z )
     
-    t_arr.append(t)
+    t_arr.append( t )
 
     # print output
     print('t= ',t, 'x=',x)
 
-print('tEnd = ', t, ' xEnd', x)
-    
-    
+print('tEnd = ', t, ' xEnd', x)    
 
 #%%
 ## POST PROCESS -> PLOTS, DATAIO
 # Store the file
-np.savetxt('xdata.dat',x_arr)
-np.savetxt('ydata.dat',y_arr)
-np.savetxt('zdata.dat',z_arr)
-np.savetxt('tdata.dat',t_arr)
+np.savetxt( 'xdata.dat', x_arr )
+np.savetxt( 'ydata.dat', y_arr )
+np.savetxt( 'zdata.dat', z_arr )
+np.savetxt( 'tdata.dat', t_arr )
 
 #load the file
-xb = np.loadtxt('xdata.dat')
-yb = np.loadtxt('ydata.dat')
-zb = np.loadtxt('zdata.dat')
-tb = np.loadtxt('tdata.dat')
+xb = np.loadtxt( 'xdata.dat' )
+yb = np.loadtxt( 'ydata.dat' )
+zb = np.loadtxt( 'zdata.dat' )
+tb = np.loadtxt( 'tdata.dat' )
 
 # make the list to arrays
-xa = np.array(x_arr)
-ya = np.array(y_arr)
-za = np.array(z_arr)
-ta = np.array(t_arr)
-
-"""
-x_arr = np.array( xa )
-y_arr = np.array( ya )
-z_arr = np.array( za )
-t_arr = np.array( ta )
-fig, axes = plt.subplots(3, 1, figsize=(10, 15))
-
-axes[0].plot(x_arr, z_arr,'r-')
-
-axes[1].plot(y_arr, z_arr, 'b-')
-
-
-axes[2].plot( (x_arr + y_arr), (x_arr - y_arr), 'k-')
-
-plt.show()"""
+xa = np.array( x_arr )
+ya = np.array( y_arr )
+za = np.array( z_arr )
+ta = np.array( t_arr )
 
 #%%
-"""
 
-# 3D plot
-ax = fig.add_subplot(111, projection='3d')
+# Plotting x vs. z
+plt.figure( figsize=(18, 6) )
+plt.subplot(1, 3, 1)
+plt.plot(xa, za, 'r-')
+plt.xlabel('X')
+plt.ylabel('Z')
+plt.title('X vs. Z')
 
-# Plot the Lorenz attractor
-ax.plot( ( x_arr + y_arr ), ( x_arr - y_arr ), z, 'r-', zdir='z')
-ax.set_xlabel('x + y')
-ax.set_ylabel('x - y')
-ax.set_zlabel('z')
+# Plotting y vs. z
+plt.subplot(1, 3, 2)
+plt.plot(ya, za, 'b-')
+plt.xlabel('Y')
+plt.ylabel('Z')
+plt.title('Y vs. Z')
 
+# Plotting x + y vs. x - y
+plt.subplot(1, 3, 3)
+plt.plot(xa + ya, xa - ya, 'g-')
+plt.xlabel('X + Y')
+plt.ylabel('X - Y')
+plt.title('X + Y vs. X - Y')
+
+plt.tight_layout()
 plt.show()
-"""
 #%%
-
 # 3-D view
 #from mpl_toolkits.mplot3d import Axes3D
-
-fig3d = plt.figure(figsize=(10,8))
-ax3d = fig3d.add_subplot(111, projection='3d')
+fig3d = plt.figure( figsize=( 10, 8 ) )
+ax3d = fig3d.add_subplot( 111, projection='3d' )
 
 ax3d.view_init( azim = 105.0, elev = 28.0 )
 
-ax3d.set_xlabel('x')
-ax3d.set_ylabel('y')
-ax3d.set_zlabel('z')
+ax3d.set_xlabel( 'x' )
+ax3d.set_ylabel( 'y' )
+ax3d.set_zlabel( 'z' )
 
 # Plot the Lorenz attractor
-ax3d.plot(xa,ya,za,'b-')
-ax3d.plot(xb,yb,zb,'r--')
-
+ax3d.plot( xa, ya, za, 'b-' )
+ax3d.plot( xb, yb, zb, 'r--' )
 
 # 2-D view
-fig2 = plt.figure(2, figsize=(10,8))
-ax = fig2.add_subplot(111)
+fig2 = plt.figure( 2, figsize=( 10, 8 ) )
+ax = fig2.add_subplot( 111 )
 
-ax.plot( xa, ya,'b-')
-ax.plot( xb, yb,'r+')
+ax.plot( xa, ya,'b-' )
+ax.plot( xb, yb,'r+' )
 
 plt.show()
-
